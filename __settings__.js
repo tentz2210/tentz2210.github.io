e90ae66f945487245c8035d5ad78354a1fdea33f
tentz2210 <tentz2210@hotmail.com>
2023-05-11 20:46:51 -0600

    Add files via upload

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+window.ASSET_PREFIX = "";
+window.SCRIPT_PREFIX = "";
+window.SCENE_PATH = "1721998.json";
+window.CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': false,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "default"
+};
+window.SCRIPTS = [ 130360871, 130360806, 130238093, 130360873, 133169380 ];
+window.CONFIG_FILENAME = "config.json";
+window.INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+window.PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/129812480/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/129812481/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/129812479/1/ammo.js', 'preload' : true},
+];
