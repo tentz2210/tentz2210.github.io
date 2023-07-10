2523bc2940bc1cea35ba73c082c01898b4295910
tentz2210 <tentz2210@hotmail.com>
2023-07-09 20:18:17 -0600

    Add files via upload

--- __settings__.js
+++ __settings__.js
@@ -3,12 +3,12 @@ window.SCRIPT_PREFIX = "";
 window.SCENE_PATH = "1721998.json";
 window.CONTEXT_OPTIONS = {
     'antialias': true,
-    'alpha': false,
+    'alpha': true,
     'preserveDrawingBuffer': false,
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-window.SCRIPTS = [ 130360871, 130360806, 130238093, 130360873, 133169380, 135421416, 137486947, 138220060, 138220319, 138240797, 138241014, 139478786 ];
+window.SCRIPTS = [ 141306260, 130360871, 130360806, 130238093, 130360873, 133169380, 135421416, 137486947, 138220060, 138220319, 138240797, 138241014, 139478786, 141501201, 141501202, 141529526, 141574495 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
